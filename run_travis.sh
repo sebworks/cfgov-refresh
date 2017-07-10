b84e3fa92a40a9bf81ff2668b215352018686473
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-10 14:43:33 -0400

    Swap to gulp cli

--- run_travis.sh
+++ run_travis.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "running $RUNTEST tests"
-ls /home/travis/.nvm/versions/node/v8.1.3/
+ls /home/travis/.nvm/versions/node/v8.1.3/bin
 if [ "$RUNTEST" == "frontend" ]; then
     gulp "test:unit"
     gulp "test:coveralls"

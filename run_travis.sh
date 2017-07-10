a33caf9a181a05aa90f9e2c4396359314bfbf858
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-10 15:57:41 -0400

    Swap to gulp cli

--- run_travis.sh
+++ run_travis.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "running $RUNTEST tests"
-$gulp=/home/travis/.nvm/versions/node/v8.1.3/lib/node_modules/gulp-cli/bin/gulp.js
+gulp=/home/travis/.nvm/versions/node/v8.1.3/lib/node_modules/gulp-cli/bin/gulp.js
 
 if [ "$RUNTEST" == "frontend" ]; then
     $gulp "test:unit"

e7421a45b14c00b6bfab9adeeb45e1dd66a130f9
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-10 22:42:39 -0400

    Fix for travis

--- install_travis.sh
+++ install_travis.sh
@@ -6,16 +6,13 @@ frontend() {
 
     # Temporarily commented out to deal with default Node version issues
     if [[ "$(node -v)" != 'v8.'* ]]; then
+        touch ~/.bash_profile
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
         source ~/.nvm/nvm.sh
         nvm install 8
-        source ~/.profile # get my PATH setup
-        source ~/.bashrc  # get my Bash aliases
     fi
 
     npm install -g gulp-cli
-
-
     chmod +x ./frontend.sh
     ./frontend.sh test
 }

c7a60dba399f7ead0bea14c2ddeb6b71c933cfad
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-11 00:43:30 -0400

    Testing node version

--- run_travis.sh
+++ run_travis.sh
@@ -5,9 +5,10 @@ set -e
 
 echo "running $RUNTEST tests"
 
+echo $PATH
 if [ "$RUNTEST" == "frontend" ]; then
     source $HOME/.nvm/nvm.sh
-    nvm use 8
+    nvm use 8.0.0
     gulp "test:unit"
     gulp "test:coveralls"
 elif [ "$RUNTEST" == "backend" ]; then
@@ -16,7 +17,7 @@ elif [ "$RUNTEST" == "backend" ]; then
     coveralls
 elif [ "$RUNTEST" == "acceptance" ]; then
     source $HOME/.nvm/nvm.sh
-    nvm use 8
+    nvm use 8.0.0
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start &
     sleep 3

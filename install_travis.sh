9d11254b5a6ec5d068918744cb598808974f22d5
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-10 23:29:25 -0400

    Fix for travis

--- install_travis.sh
+++ install_travis.sh
@@ -9,6 +9,7 @@ frontend() {
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
         source $HOME/.nvm/nvm.sh
         nvm install 8
+        export PATH=/home/travis/.nvm/versions/node/:$PATH
         source ~/.bash_profile
     fi
 

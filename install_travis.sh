2206e964cc5c6995777cde19d868bb1fa5706902
Steven Bellamy <steven.bellamy@cfpb.gov>
2017-07-11 00:24:19 -0400

    Testing node version

--- install_travis.sh
+++ install_travis.sh
@@ -8,11 +8,13 @@ frontend() {
     if [[ "$(node -v)" != 'v8.'* ]]; then
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
         source $HOME/.nvm/nvm.sh
-        nvm install 8
 
+        nvm install 8
+        export PATH=/home/travis/.nvm/versions/node/v8.1.3:$PATH
+        source ~/.bash_profile
     fi
+
     npm install -g gulp-cli
-    source ~/.bash_profile
     chmod +x ./frontend.sh
 
     ./frontend.sh test
